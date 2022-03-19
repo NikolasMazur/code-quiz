3d542e08f4aca4f69d9c9db140764ad3d2b2e6d2
Nikolas Mazur <kolemazur@gmail.com>
2022-03-19 15:29:11 -0600

    Reformat questions and index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,66 +4,53 @@ var timeLeft = 60;
 var quizQuestions = document.querySelector("#container");
 var defaultTime;
 var currentQuestionIndex = 0;
-const questions = [
-    {
+var questions = [{
         question: "What does DOM stand for?",
-        options: {
-            a: "Disfunctional Operation Module",
-            b: "Document of Module",
-            c: "Document Object Model",
-            d: "Data Oriented Model"
-        },
-    answer: "c"
-    },
-    {
+        a: "Disfunctional Operation Module",
+        b: "Document of Module",
+        c: "Document Object Model",
+        d: "Data Oriented Model",
+        answer: "c"
+},
+{
         question: "Java is the same as JavaScript.",
-        options: {
-            a: "True",
-            b: "False"
-        },
-    answer: "b"
-    },
-    {
+        a: "True",
+        b: "False",
+        answer: "b"
+},
+{
         question: "Which of the following is an invalid function name?",
-        options: {
-            a: "start",
-            b: "toCelsius",
-            c: "return",
-            d: "print"
-        },
-    answer: "c"
-    },
-    {
+        a: "start",
+        b: "toCelsius",
+        c: "return",
+        d: "print",
+        answer: "c"
+},
+{
         question: "Which of the following is false?",
-        options: {
-            a: "Variables defined with 'const' cannot be Redecared.",
-            b: "Variables defied with 'const' cannot be reassigned.",
-            c: "Variables defined with 'const' have Block Scope.",
-            d: "Variables defined with 'const' don't need to have a value assigned when declared."
-        },
-    answer: "d"
-    },
-    {
+        a: "Variables defined with 'const' cannot be Redecared.",
+        b: "Variables defied with 'const' cannot be reassigned.",
+        c: "Variables defined with 'const' have Block Scope.",
+        d: "Variables defined with 'const' don't need to have a value assigned when declared.",
+        answer: "d"
+},
+{
         question: "How would you access the first element in an array?",
-        options: {
-            a: "array[1]",
-            b: "array[0]",
-            c: "array[-1]",
-            d: "array[]"
-        },
-    answer: "b"
-    },
-    {
+        a: "array[1]",
+        b: "array[0]",
+        c: "array[-1]",
+        d: "array[]",
+        answer: "b"
+},
+{
         question: "Which of the following are invalid data types?",
-        options: {
-            a: "Float",
-            b: "String",
-            c: "Object",
-            d: "None of the above"
-        },
-    answer: "a"
-    },
-]
+        a: "Float",
+        b: "String",
+        c: "Object",
+        d: "None of the above",
+        answer: "a"
+},
+];
 
 // Create timer and countdown
 function timer() {
@@ -85,3 +72,11 @@ function  adjustTime(amount) {
 }
 startQuiz.onclick = timer;
 
+// var score = 0;
+// document.getElementById("answersCheck").onclick = function() {
+//     var answers = document.getElementById("answers").value;
+// }
+
+// if (score === "true") {
+//     score++;
+// }
